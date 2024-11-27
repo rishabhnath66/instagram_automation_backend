686af689d80bcd484facb266e5f6d88f02d3981c
unknown <rishabh.nath@pixelnx.com>
2024-11-27 18:10:38 +0530

    comment cron

--- src/jobs/cron.js
+++ src/jobs/cron.js
@@ -1,10 +1,9 @@
 let cron = require('node-cron');
 const { initSchedule } = require('../services/schedulepost');
 const { initPostEngagement } = require('../services/postEngagement');
-let task =cron.schedule('*/6 * * * * *', () => {
-    initSchedule()
+cron.schedule('*/6 * * * * *', () => {
+    // initSchedule()
 });
-let task1 =cron.schedule('*/6 * * * * *', () => {
-    initPostEngagement()
+cron.schedule('*/6 * * * * *', () => {
+    // initPostEngagement()
 });
-task.start()
